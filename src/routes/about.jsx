6a74d648160650bc91d3e52afa4e28b225e10944
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-11-23 14:21:41 +0200

    routes

---
+++ src/routes/about.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import "../App.css";
+
+function About () {
+    return (
+        <main >
+        <div className="home">
+       <h2>Something about</h2>
+        </div></main>
+    );
+}
+
+export default About;
