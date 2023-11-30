cd7c14ba5032819f11f0dd5c52fbb4c427b201a2
Inna Kozhina <inna.kozhina22@gmail.com>
2023-11-30 14:09:06 +0200

    function to get receipts by ingredients

---
+++ src/receipt.js
@@ -0,0 +1,10 @@
+function fetchData(ingredients) {
+  fetch(
+    `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=10&apiKey=bea3c85afe2346a6810cb7168710f978`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+fetchData();
