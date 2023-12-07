ec7180fe585ca93217d7e707d3fd313fc31a774f
Inna Kozhina <inna.kozhina22@gmail.com>
2023-12-07 12:05:31 +0200

    set RouterProvider, add header

--- src/routes/error.jsx
+++ src/routes/error.jsx
@@ -1,13 +1,17 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import "../App.css";
 
-export default function ErrorPage () {
-    const error = useRouteError(); 
-    return (
-        <main>
-        <h2 className="back">Ups, something went wrong</h2>
-        <p>{error.statusText || error.message}</p>
-        <p className="back"><a href='/'><div>TO HOME PAGE</div></a></p>
-        </main>
-    )
-};
+export default function ErrorPage() {
+  const error = useRouteError();
+  return (
+    <main>
+      <h2 className="back">Ups, something went wrong</h2>
+      <p>{error.statusText || error.message}</p>
+      <p className="back">
+        <Link href="/">
+          <div>TO HOME PAGE</div>
+        </Link>
+      </p>
+    </main>
+  );
+}
